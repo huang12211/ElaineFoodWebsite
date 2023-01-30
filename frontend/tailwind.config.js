7bbff39b6e9f625246ffd1035b294e5014ae475c
Replit user <>
2023-01-30 06:04:19 +0000

    top nav bar functionnal

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["{html,js}"],
   theme: {
+    colors: {
+      'yellow': '#FFD77D',
+      'darkYellow': '#FEC23A',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
     extend: {},
   },
   plugins: [],
