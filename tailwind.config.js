22b643a789cde20abea83a18da6c78540b118af7
Replit user <>
2023-02-19 23:29:17 +0000

    Perfectly alighed top bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,6 @@ module.exports = {
     container: {
       center: true,
     },
-
     fontSize: {
       sm: '0.8rem',
       base: '1rem',
@@ -25,57 +24,40 @@ module.exports = {
       '5xl': '3.052rem',
     },
 
-    width: {
-      '8': '2rem',
-      '10': '2.5rem',
-      '12': '3rem',
-      '14': '3.5rem',
-      '16': '4rem',
-      '18': '4.5rem',
-      '44': '11rem',
-      '128': '36rem',
-      '20': '5rem',
-    },
 
-    maxWidth: {
-      '8': '2rem',
-      '10': '2.5rem',
-      '12': '3rem',
-      '14': '3.5rem',
-      '16': '4rem',
-      '18': '4.5rem',
-      '20': '5rem',
-    },
 
-    height: {
-      '8': '2rem',
-      '10': '2.5rem',
-      '12': '3rem',
-      '14': '3.5rem',
-      '16': '4rem',
-      '18': '4.5rem',
-      '20': '5rem',
-    },
-
-    maxHeight: {
-      '8': '2rem',
-      '10': '2.5rem',
-      '12': '3rem',
-      '14': '3.5rem',
-      '16': '4rem',
-      '18': '4.5rem',
-      '20': '5rem',
-    },
 
     extend: {
+
       colors: {
         'paleYellow': '#FFD77D',
         'darkYellow': '#FEC23A',
       },
+
       backgroundImage: {
         'kitchen-counter': "url('https://elainefoodwebsite.ehuang3.repl.co/media/Generic%20Images/FeaturedRecipeBackground.png')",
       },
+
+      width: {
+        '8': '2rem',
+        '10': '2.5rem',
+        '12': '3rem',
+        '14': '3.5rem',
+        '16': '4rem',
+        '18': '4.5rem',
+        '44': '11rem',
+        '128': '36rem',
+        '20': '5rem',
+      },
+
       maxWidth: {
+        '8': '2rem',
+        '10': '2.5rem',
+        '12': '3rem',
+        '14': '3.5rem',
+        '16': '4rem',
+        '18': '4.5rem',
+        '20': '5rem',
         '36': '9rem',
         '40': '10rem',
         '44': '11rem',
@@ -85,6 +67,26 @@ module.exports = {
         '60': '15rem',
         '64': '16rem',
         '72': '18rem',
+      },
+
+      height: {
+        '8': '2rem',
+        '10': '2.5rem',
+        '12': '3rem',
+        '14': '3.5rem',
+        '16': '4rem',
+        '18': '4.5rem',
+        '20': '5rem',
+      },
+
+      maxHeight: {
+        '8': '2rem',
+        '10': '2.5rem',
+        '12': '3rem',
+        '14': '3.5rem',
+        '16': '4rem',
+        '18': '4.5rem',
+        '20': '5rem',
       },
     },
   },
